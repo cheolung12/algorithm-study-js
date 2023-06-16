a666fb652d845478ae9d67c59d42448bfa768b74
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-06-16 23:35:16 +0900

    23.06.16

--- programmers/high_score/dfs/bfs/타겟넘버.js
+++ programmers/high_score/dfs/bfs/타겟넘버.js
@@ -1,4 +1,6 @@
-let answer = 0;
+// 주어진 배열의 모든 원소에 대해 더하거나 빼는 모든 경우의 수를 찾는다.
+
+let answer = 0; // 전역변수 선언
 
 function solution(numbers, target) {
     dfs(0, 0, numbers, target);
@@ -6,11 +8,13 @@ function solution(numbers, target) {
 }
 
 const dfs = (idx, value, numbers, target) => {
+    // 종료 조건
     if (idx === numbers.length){
         if (value === target)
             answer += 1;
         return 
     }  
+    // 연산은 재귀에서
     dfs(idx+1, value + numbers[idx], numbers, target);
     dfs(idx+1, value - numbers[idx], numbers, target);
 }
