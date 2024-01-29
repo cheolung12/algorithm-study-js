1200bc7e050c18e6b1ca627af562f7f50b974c46
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-01-29 23:05:01 +0900

    23.01.29

--- programmers/lv2/방금그곡.js
+++ programmers/lv2/방금그곡.js
@@ -1,30 +1,19 @@
-function solution(m, musicinfos) {
-  const ans = [];
+const solution = (m, musicInfos) => {
+    let answer = '';
 
-  for (let musicinfo of musicinfos) {
-    const [start, end, title, music] = musicinfo.split(',');
+    musicInfos = musicInfos.map(e => {
+        let eArr = e.split(',');
+        let timeDiff = (new Date(`1970-01-01 ${eArr[1]}:00`) - new Date(`1970-01-01 ${eArr[0]}:00`)) / 60000;
+        let melody = eArr[3].replace(/[A-Z]#/g,m => m[0].toLowerCase());
+        melody = melody.repeat(Math.ceil(timeDiff / melody.length)).substr(0, timeDiff);
+        return `${timeDiff},${eArr[2]},${melody}`;
+    });
 
-    // 총 음악 재생 시간 구하기
-    const [startH, startM] = start.split(':');
-    const [endH, endM] = end.split(':');
-    const musicTime = endH * 60 + Number(endM) - (startH * 60 + Number(startM));
+    musicInfos.sort((a,b) => b.split(',')[0] - a.split(',')[0]);
 
-    // 총 재생 시간에 맞춰서 멜로디 나열
-    let idx = 0,
-      count = 0;
-    let melody = '';
-    while (count < musicTime) {
-      melody += music[idx % music.length];
-      if (music[idx % music.length] !== '#') {
-        count++;
-      }
-      idx++;
-    }
+    answer = musicInfos.filter(e => e.split(',')[2].indexOf(m.replace(/[A-Z]#/g,m => m[0].toLowerCase())) != -1);
 
-    if (melody.includes(m) && melody[melody.indexOf(m) + m.length] !== '#')
-      ans.push([title, musicTime]);
-  }
-
-  if (ans.length === 0) return '(None)';
-  else return ans.sort((a, b) => b[1] - a[1])[0][0];
+    return answer.length == 0 ? '(None)' : answer[0].split(',')[1];
 }
+
+// new Date로 시간차를 좀 더 간편하게 구하고 replace함수를 통해 처리하기 힘든 문자열을 처리해주자 
